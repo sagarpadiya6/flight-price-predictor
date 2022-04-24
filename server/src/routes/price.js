766b387bec9207fb092ba3ebe9ee36df1e49ce3b
Sagar Padiya <sagarpadiya7000@gmail.com>
2022-04-25 02:38:41 +0530

    added price route

--- server/src/routes/price.js
+++ server/src/routes/price.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import { spawn } from 'child_process';
+import path from 'path';
+import { once } from 'events';
 import { User, Review } from '../models';
 import verifyToken from '../middlewares/verifyToken';
-import { errorMsg } from '../constants';
+import { errorMsg, ERROR_CODE } from '../constants';
 
 /**
  * @swagger
@@ -22,26 +25,62 @@ const router = express.Router();
  *       200:
  *         description: Predicted flight price sent successfully
  */
-router.get('/', verifyToken, async (req, res) => {
-  const { userUuid: uuid } = req;
-  const { review, star, recommendation } = req.body;
+router.post('/', verifyToken, async (req, res) => {
+  const { userUuid: loggedInUser } = req;
+  const { depDateTime, arrDateTime, stops, airline, source, destination } =
+    req.body;
 
+  console.log({
+    depDateTime,
+    arrDateTime,
+    stops,
+    airline,
+    source,
+    destination,
+  });
   try {
-    const user = await User.findOne({ where: { uuid } });
+    const user = await User.findOne({ where: { uuid: loggedInUser.id } });
     if (!user) {
       return errorMsg(res, 400, ERROR_CODE.ERR_USER_NOT_FOUND);
     }
-    const userReview = await Review.create({
-      star,
-      review,
-      recommendation,
-      userId: user.id,
+
+    const py = spawn('python3', [
+      // "-m",
+      path.resolve(__dirname, '../../../ai/flight_price/flight_price.py'),
+      '--dep_date_time=' + depDateTime,
+      '--arr_date_time=' + arrDateTime,
+      '--stops=' + stops,
+      '--airline=' + airline,
+      '--source=' + source,
+      '--destination=' + destination,
+      // "--debug",
+    ]);
+    let output = '';
+
+    py.stdin.setEncoding = 'utf-8';
+
+    py.stdout.on('data', (data) => {
+      output += data.toString();
+      // console.log("output was generated: " + output);
+    });
+    // Handle error output
+    py.stderr.on('data', (data) => {
+      // As said before, convert the Uint8Array to a readable string.
+      // console.log("error:" + data);
+    });
+    py.stdout.on('end', async function (code) {
+      // console.log("output: " + output);
+      // console.log(`Exit code is: ${code}`);
     });
 
-    return res.status(200).json({ review: userReview });
+    await once(py, 'close');
+
+    console.log('output: ' + output);
+
+    return res.status(200).json({ price: JSON.parse(output) });
   } catch (err) {
     console.log(err);
-    return res.status(500).json(err);
+    return errorMsg(res, 500, ERROR_CODE.ERR_SOMETHING_WENT_WRONG, err);
   }
 });
 
